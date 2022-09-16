2d0d174095ffb7b34808cbd48b1bf6471d7667ed
Udoka <udochukwukaonyela@gmail.com>
2022-09-16 23:34:22 +0100

    added logic for checkers and event listener

--- index.js
+++ index.js
@@ -32,6 +32,28 @@ helpers.BookCode = (strLength) => {
 };
 helpers.getElement = (attribute) => document.querySelector(`[data-element = ${attribute}]`);
 helpers.getElementById = (attribute) => document.querySelector(`#${attribute}`);
+helpers.createBooking = ({ id, type, status, car, startDate, endDate }) => { 
+    const div = document.createElement("div");
+    div.className = "w-full shadow-lg overflow-x-scroll gap-6 shadow-gray-300/25 h-auto py-6 flex justify-between items-center rounded-md lg:text-sm text-xs px-6 text-gray-600";
+    div.innerHTML = `
+                        <p class="self-start basis-1/6 align-middle my-auto min-w-max">${id}</p>
+                        <p class="self-start basis-1/6 align-middle my-auto min-w-max"><span
+                                class="${type === "Car Rental" ? "approved" : status === "Processing" ? "processing" : status === "Secured" ? "secured" : ""} self-start lg:w-fit px-5 py-1 flex items-center justify-center rounded-full font-semibold w-max">${status}</span>
+                        </p>
+                        <p class="self-start basis-1/6 align-middle my-auto min-w-max">${type}</p>
+                        <p class="self-start basis-1/6 align-middle my-auto min-w-max">${car}</p>
+                        <p class="self-start basis-1/6 align-middle my-auto min-w-max">${startDate}</p>
+                        <p class="self-start basis-1/6 align-middle my-auto min-w-max">${endDate}</p>
+    `
+    tableContainer.appendChild(div);
+};
+helpers.createError = (str) => {
+    const p = document.createElement("p");
+    p.className = "text-xs text-red-400 transition-transform origin-left duration-500 tracking-wider";
+    p.innerText = str;
+    return p;
+}
+let bookings = [];
 const bookButton = helpers.getElement("book-button");
 const modal = helpers.getElement("modal");
 const closeModal = helpers.getElement("close-modal");
@@ -42,6 +64,11 @@ const bookingStartDate = helpers.getElementById("booking-start-date");
 const bookingEndDate = helpers.getElementById("booking-end-date");
 const tableContainer = helpers.getElement("table-container");
 const submitButton = helpers.getElement("submit-button");
+const modalContainer = helpers.getElement("modal-container");
+const startDateContainer = helpers.getElement("start-date-container");
+const endDateContainer = helpers.getElement("end-date-container");
+const bookingTypeContainer = helpers.getElement("booking-type-container");
+const bookingCarContainer = helpers.getElement("booking-car-container");
 bookButton.addEventListener("click", () => {
     modal.classList.toggle("scale-100");
     modal.classList.toggle("scale-0");
@@ -51,3 +78,95 @@ closeModal.addEventListener("click", () => {
     modal.classList.toggle("scale-100");
     modal.classList.toggle("scale-0");
 });
+
+submitButton.addEventListener("click", () => { 
+    if (!!bookingId.value && !!bookingType.value && !!bookingCar.value && !!bookingStartDate.value && !!bookingEndDate.value) {
+        if (bookingStartDate.value < bookingEndDate.value) {
+            if (bookingStartDate.value > new Date().toISOString().split("T")[0]) {
+                    const booking = {
+                        id: bookingId.value,
+                        type: bookingType.value,
+                        status: bookingType.value === "Car Rental" ?  "Coming Soon" : bookings.length > 20 ? "Processing" : "Secured",
+                        car: bookingCar.value,
+                        startDate: bookingStartDate.value,
+                        endDate: bookingEndDate.value
+                    };
+                    bookings = [...bookings, booking];
+                helpers.createBooking(booking);
+                modal.classList.toggle("scale-100");
+                modal.classList.toggle("scale-0");
+                bookingId.value = "";
+                bookingType.value = "";
+                bookingCar.value = "";
+                bookingStartDate.value = "";
+                bookingEndDate.value = "";
+                modalContainer.scrollTo(0, 0);
+            } else {
+                const startDateContainerMessage = helpers.createError("Start date must be greater than today's date");
+                startDateContainer.appendChild(startDateContainerMessage);
+                setTimeout(() => {
+                    startDateContainerMessage.classList.add("scale-0");
+                    setTimeout(() => {
+                        startDateContainer.removeChild(startDateContainerMessage)
+                    }, 1000);
+                }, 5000);
+            }
+        } else {
+            const startDateContainerMessage = helpers.createError("Start date must be less than end date");
+            const endDateContainerMessage = helpers.createError("End date must be greater than start date");
+            startDateContainer.appendChild(startDateContainerMessage);
+            endDateContainer.appendChild(endDateContainerMessage);
+            setTimeout(() => {
+                startDateContainerMessage.classList.add("scale-0");
+                endDateContainerMessage.classList.add("scale-0");
+                setTimeout(() => {
+                    startDateContainer.removeChild(startDateContainerMessage)
+                    endDateContainer.removeChild(endDateContainerMessage);
+                }, 1000);
+            }, 5000);
+        }
+    }
+    else {
+        if (!bookingType.value) {
+            const bookingTypeContainerMessage = helpers.createError("Booking Type is required");
+            bookingTypeContainer.appendChild(bookingTypeContainerMessage);
+            setTimeout(() => {
+                bookingTypeContainerMessage.classList.add("scale-0");
+                setTimeout(() => {
+                    bookingTypeContainer.removeChild(bookingTypeContainerMessage)
+                }, 1000);
+            }, 5000);
+        }
+        if (!bookingCar.value) {
+            const bookingCarContainerMessage = helpers.createError("Booking Car is required");
+            bookingCarContainer.appendChild(bookingCarContainerMessage);
+            setTimeout(() => {
+                bookingCarContainerMessage.classList.add("scale-0");
+                setTimeout(() => {
+                    bookingCarContainer.removeChild(bookingCarContainerMessage)
+                }, 1000);
+            }, 5000);
+        }
+        if (!bookingStartDate.value) {
+            const startDateContainerMessage = helpers.createError("Start date is required");
+            startDateContainer.appendChild(startDateContainerMessage);
+            setTimeout(() => {
+                startDateContainerMessage.classList.add("scale-0");
+                setTimeout(() => {
+                    startDateContainer.removeChild(startDateContainerMessage)
+                }, 1000);
+            }, 5000);
+        }
+        if (!bookingEndDate.value) {
+            const endDateContainerMessage = helpers.createError("End date is required");
+            endDateContainer.appendChild(endDateContainerMessage);
+            setTimeout(() => {
+                endDateContainerMessage.classList.add("scale-0");
+                setTimeout(() => {
+                    endDateContainer.removeChild(endDateContainerMessage)
+                }, 1000);
+            }, 5000);
+        }
+    }
+});
+
